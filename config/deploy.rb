0503b55630a7213fda508c99824815f6c9cbd2ec
Rick Smoke <rsmoke@umich.edu>
2021-02-14 17:22:40 -0500

    remove namespace

--- config/deploy.rb
+++ config/deploy.rb
@@ -93,15 +93,15 @@ namespace :puma do
   before :start, :make_dirs
 end
 
-namespace :check do
-  before :linked_files, :set_master_key do
-    on roles(:app), in: :sequence, wait: 10 do
-      unless test("[ -f #{shared_path}/config/master.key ]")
-        upload! 'config/master.key', "#{shared_path}/config/master.key"
-      end
-    end
-  end
-end
+# namespace :check do
+#   before :linked_files, :set_master_key do
+#     on roles(:app), in: :sequence, wait: 10 do
+#       unless test("[ -f #{shared_path}/config/master.key ]")
+#         upload! 'config/master.key', "#{shared_path}/config/master.key"
+#       end
+#     end
+#   end
+# end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
