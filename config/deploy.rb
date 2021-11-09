1f0e9aed9b1dd4318d28c8414639923cde7391c2
Rick Smoke <rsmoke@umich.edu>
2021-11-09 11:45:43 -0500

    change branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url, "git@github.com:lsa-mis/nelp_application.git"
 set :user, "deployer"
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :branch, "main"
+set :branch, "master"
 
 # Don't change these unless you know what you're doing
 set :pty,             true
