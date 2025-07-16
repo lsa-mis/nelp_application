21cb9607b11e680360d5e3bfe2352fd5f71b905b
rsmokeUM <rsmoke@umich.edu>
2025-07-16 09:18:18 -0400

    Refactor code structure for improved readability and maintainability

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: rails server -p 3000
-webpack: NODE_OPTIONS=--openssl-legacy-provider bin/webpack-dev-server
--- app/javascript/packs/active_admin.js
+++
@@ -1,8 +0,0 @@
-// Load Active Admin's styles into Webpacker,
-// see `active_admin.scss` for customization.
-//= require active_admin/base
-//= require trix/dist/trix
-import "../stylesheets/active_admin";
-
-import "@activeadmin/activeadmin";
-import 'trix/dist/trix'
