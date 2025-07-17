9472ea344629ff7c65722632460a9e60d9984a61
rsmokeUM <rsmoke@umich.edu>
2025-07-17 13:46:23 -0400

    Add yarn.lock file with dependencies for Bootstrap and Popper.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 import "@hotwired/turbo-rails"
+import * as bootstrap from "bootstrap"
+
+// Make Bootstrap globally available if needed
+window.bootstrap = bootstrap
