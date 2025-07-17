497a12e56f00b0c55e4b29897c163c712d96413d
rsmokeUM <rsmoke@umich.edu>
2025-07-17 14:14:15 -0400

    Update asset management and dependencies; remove unused Procfile, add CSS build scripts, and configure Bootstrap integration

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "bootstrap" # @5.3.7
+pin "bootstrap", to: "bootstrap.bundle.min.js"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # No Sprockets configuration needed with cssbundling-rails only
+Rails.application.config.assets.precompile << "bootstrap.bundle.min.js"
