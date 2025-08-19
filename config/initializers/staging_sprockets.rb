9ab98db9051b3309b77c37d3a26b528d7b8ea5fe
rsmokeUM <rsmoke@umich.edu>
2025-08-19 11:55:59 -0400

    Update staging Sprockets initializer to run SCSS processing configuration after initialization, ensuring proper asset management in the staging environment.

--- config/initializers/staging_sprockets.rb
+++ config/initializers/staging_sprockets.rb
@@ -1,8 +1,8 @@
 # Disable SCSS processing in Sprockets for staging environment
 if Rails.env.staging?
-  # This runs before Sprockets tries to process any files
-  Rails.application.config.before_initialize do
-    if defined?(Sprockets)
+  # This runs after Sprockets has been initialized
+  Rails.application.config.after_initialize do
+    if defined?(Sprockets) && Rails.application.assets
       Rails.application.assets.configure do |env|
         # Remove any SCSS processors
         if defined?(Sprockets::SasscProcessor)
